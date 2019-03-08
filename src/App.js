543e49cf6fd5609ee4cff7551ea6744027080c7c
code1031 <1031247267@qq.com>
2019-03-08 16:44:52 +0800

    完成全局路由配置

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
+
+function Login() {
+  return <div>Login</div>;
+}
+
+function Main() {
+  return <div>Main</div>;
+}
 
 class App extends Component {
   render() {
     return (
-      <div>
-        初始化项目
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Login}/>
+          <Route path="/home" component={Main}/>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
