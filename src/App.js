9dee2a6cd391ae5549ec771ef623b83a7e1f5144
code1031 <1031247267@qq.com>
2019-03-08 16:10:42 +0800

    初始化第一个版本

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        初始化项目
+      </div>
+    );
+  }
+}
+
+export default App;
