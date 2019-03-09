2028e8e7d0003661359e07ef1d410cc8fa65ebff
code1031 <1031247267@qq.com>
2019-03-09 17:15:28 +0800

    完成主页子路由配置-有bug

--- src/module/main.js
+++ src/module/main.js
@@ -1,6 +1,8 @@
 // 整体布局
 import React from 'react';
-import { Grid } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react';
+import { Route, Link, Switch } from "react-router-dom";
+
 import './main.css';
 
 class Main extends React.Component {
@@ -8,34 +10,47 @@ class Main extends React.Component {
     return (
       <div className='home-container'>
         <div className="home-content">
-
+          <Switch>
+            <Route path='/home/main' render={()=><div>home</div>}/>
+            <Route path='/home/info' render={()=><div>info</div>}/>
+            <Route path='/home/chat' render={()=><div>chat</div>}/>
+            <Route path='/home/my' render={()=><div>my</div>}/>
+          </Switch>
         </div>
         <div className="home-menu">
           <Grid columns={4} divided>
             <Grid.Row>
               <Grid.Column>
-                <div className="placeholder">
-                  <i className='iconfont icon-all'></i>
-                  <div>菜单</div>
-                </div>
+                <Link to='/home/main'>
+                  <div className="placeholder">
+                    <i className='iconfont icon-all'></i>
+                    <div>菜单</div>
+                  </div>
+                </Link>
               </Grid.Column>
               <Grid.Column>
-                <div className="placeholder">
-                  <i className='iconfont icon-search'></i>
-                  <div>菜单</div>
-                </div>
+                <Link to='/home/info'>
+                  <div className="placeholder">
+                    <i className='iconfont icon-all'></i>
+                    <div>菜单</div>
+                  </div>
+                </Link>
               </Grid.Column>
               <Grid.Column>
-                <div className="placeholder">
-                  <i className='iconfont icon-all'></i>
-                  <div>菜单</div>
-                </div>
+                <Link to='/home/chat'>
+                  <div className="placeholder">
+                    <i className='iconfont icon-all'></i>
+                    <div>菜单</div>
+                  </div>
+                </Link>
               </Grid.Column>
               <Grid.Column>
-                <div className="placeholder">
-                  <i className='iconfont icon-all'></i>
-                  <div>菜单</div>
-                </div>
+                <Link to='/home/my'>
+                  <div className="placeholder">
+                    <i className='iconfont icon-all'></i>
+                    <div>菜单</div>
+                  </div>
+                </Link>
               </Grid.Column>
             </Grid.Row>
           </Grid>
