2e7599e0cc2d1a12873dd8fd1b4cb5cc5872ef25
code1031 <1031247267@qq.com>
2019-03-09 17:51:19 +0800

    完成菜单链接样式的动态控制

--- src/module/main.js
+++ src/module/main.js
@@ -5,6 +5,26 @@ import { Route, Link, Switch } from "react-router-dom";
 
 import './main.css';
 
+// 自定义链接样式:只要有链接触发，children对应的函数就会调用
+function Menu(props) {
+  let {to, mname, icon} = props;
+  return (
+    <Route path={to} children={({match})=>{
+      // 匹配路径之后，math是对象，否则是null
+      let iconClass = 'iconfont icon-' + icon;
+      iconClass = match? iconClass + ' active': iconClass;
+      return (
+        <Link to={to}>
+          <div className="placeholder">
+            <i className={iconClass}></i>
+            <div className={match?'active':''}>{mname}</div>
+          </div>
+        </Link>
+      );
+    }}/>
+  );
+}
+
 class Main extends React.Component {
   render() {
     return (
@@ -21,36 +41,16 @@ class Main extends React.Component {
           <Grid columns={4} divided>
             <Grid.Row>
               <Grid.Column>
-                <Link to='/home/main'>
-                  <div className="placeholder">
-                    <i className='iconfont icon-all'></i>
-                    <div>菜单</div>
-                  </div>
-                </Link>
+                <Menu to='/home/main' mname='主页' icon='all'/>
               </Grid.Column>
               <Grid.Column>
-                <Link to='/home/info'>
-                  <div className="placeholder">
-                    <i className='iconfont icon-all'></i>
-                    <div>菜单</div>
-                  </div>
-                </Link>
+                <Menu to='/home/info' mname='资讯' icon='search'/>
               </Grid.Column>
               <Grid.Column>
-                <Link to='/home/chat'>
-                  <div className="placeholder">
-                    <i className='iconfont icon-all'></i>
-                    <div>菜单</div>
-                  </div>
-                </Link>
+                <Menu to='/home/chat' mname='微聊' icon='atm'/>
               </Grid.Column>
               <Grid.Column>
-                <Link to='/home/my'>
-                  <div className="placeholder">
-                    <i className='iconfont icon-all'></i>
-                    <div>菜单</div>
-                  </div>
-                </Link>
+                <Menu to='/home/my' mname='我的' icon='account'/>
               </Grid.Column>
             </Grid.Row>
           </Grid>
