9209f428cab96a8c345fda03fab9991333db34e4
code1031 <1031247267@qq.com>
2019-03-12 16:06:12 +0800

    完成计算器选项卡布局

--- src/module/home/calc.js
+++ src/module/home/calc.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Icon } from 'semantic-ui-react';
+import { Icon, Tab } from 'semantic-ui-react';
 
 class Calc extends React.Component {
   render() {
+    const panes = [
+      { menuItem: '公积金贷款', render: () => <Tab.Pane>公积金贷款</Tab.Pane> },
+      { menuItem: '商业贷款', render: () => <Tab.Pane>商业贷款</Tab.Pane> },
+      { menuItem: '组合贷款', render: () => <Tab.Pane>组合贷款</Tab.Pane> },
+    ]
     return (
       <div className='home-calc'>
         <div className = "home-calc-title">
           <Icon name = 'angle left' size = 'large'/>贷款利率计算 
         </div> 
         <div className = "map-calc-content">
-          计算器
+          <Tab panes={panes}/>
         </div>
       </div>
     );
