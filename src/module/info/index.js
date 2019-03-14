7a46e820738173947a0330ca20a2eb2ec8da8333
code1031 <1031247267@qq.com>
2019-03-14 17:26:53 +0800

    添加第三方加载更多组件

--- src/module/info/index.js
+++ src/module/info/index.js
@@ -2,70 +2,89 @@ import React from 'react';
 import { Tab, Item } from 'semantic-ui-react';
 import './index.css';
 import axios from 'axios';
+import LoadMore from './loadmore';
 
-class InfoContent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      infoData: [],
-      pagenum: 0,
-      pagesize: 2,
-      type: 1
-    }
-  }
-  componentDidMount = async () => {
-    // 加载数据
-    let ret = await axios.post('infos/list', {
-      pagenum: this.state.pagenum,
-      pagesize: this.state.pagesize,
-      type: this.state.type
-    });
-    this.setState({
-      infoData: ret.data.data.list.data
-    });
-  }
-  render() {
-    let infoContent = this.state.infoData.map(item=>{
-      return (
-        <Item key={item.id}>
-          <Item.Image size='small' src='http://47.96.21.88:8086/public/1.png' />
-          <Item.Content verticalAlign='middle'>
-            <Item.Header className='info-title'>{item.info_title}</Item.Header>
-            <Item.Meta>
-              <span className='price'>$1200</span>
-              <span className='stay'>1 Month</span>
-            </Item.Meta>
-          </Item.Content>
-        </Item>
-      );
-    });
-    return (
-      <div>
-        {infoContent}
-      </div>
-    );
-  }
-}
+// class InfoContent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       infoData: [],
+//       pagenum: 0,
+//       pagesize: 2,
+//       type: 1
+//     }
+//   }
+//   componentDidMount = async () => {
+//     // 加载数据
+//     let ret = await axios.post('infos/list', {
+//       pagenum: this.state.pagenum,
+//       pagesize: this.state.pagesize,
+//       type: this.state.type
+//     });
+//     this.setState({
+//       infoData: ret.data.data.list.data
+//     });
+//   }
+//   render() {
+//     let infoContent = this.state.infoData.map(item=>{
+//       return (
+//         <Item key={item.id}>
+//           <Item.Image size='small' src='http://47.96.21.88:8086/public/1.png' />
+//           <Item.Content verticalAlign='middle'>
+//             <Item.Header className='info-title'>{item.info_title}</Item.Header>
+//             <Item.Meta>
+//               <span className='price'>$1200</span>
+//               <span className='stay'>1 Month</span>
+//             </Item.Meta>
+//           </Item.Content>
+//         </Item>
+//       );
+//     });
+//     return (
+//       <div>
+//         {infoContent}
+//       </div>
+//     );
+//   }
+// }
 
-class TopContent extends React.Component {
-  render() {
-    return (
-      <div>
-        头条内容
-      </div>
-    );
-  }
+function InfoContent() {
+  let param = {
+    type: 1
+  };
+  return <LoadMore param={param}/>;
 }
+function TopContent() {
+  let param = {
+    type: 2
+  };
+  return <LoadMore param={param}/>;
+}
+function FaqContent() {
+  let param = {
+    type: 3
+  };
+  return <LoadMore param={param}/>;
+}
+// class TopContent extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         头条内容
+//       </div>
+//     );
+//   }
+// }
 
-class FaqContent extends React.Component {
-  render() {
-    return (
-      <div>
-        问答内容
-      </div>
-    );
-  }
-}
+// class FaqContent extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         问答内容
+//       </div>
+//     );
+//   }
+// }
 
 class Info extends React.Component {
   render() {
---
+++ src/module/info/loadmore.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import Tloader from 'react-touch-loader';
+import { Item } from 'semantic-ui-react';
+import './loadmore.css';
+import axios from 'axios';
+
+class LoadMore extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasMore: true,   // 是否还有更多数据
+      initializing: 1, // 开始进度条
+      pagenum: 0,      // 开始的记录条数
+      pagesize: 2,     // 每次加载条数
+      listData: [],    // 列表数据
+      total: 0         // 列表总长度
+    }
+  }
+
+  componentDidMount = async () => {
+    let {param} = this.props;
+    let {pagenum, pagesize} = this.state;
+    // 加载数据列表
+    let ret = await axios.post('infos/list', {
+      type: param.type,
+      pagenum: pagenum,
+      pagesize: pagesize
+    });
+    // 更新数据
+    this.setState({
+      listData: ret.data.data.list.data,
+      total: ret.data.data.list.total
+    });
+  }
+
+  // 实现刷新操作
+  refresh = (resolve) => {
+    console.log('refresh')
+  }
+
+  // 实现加载更多操作
+  loadMore = (resolve) => {
+    console.log('loadMore')
+  }
+
+  // 生成列表内容
+  produceList = (type) => {
+    let listContent = [];
+    this.state.listData.forEach(item=>{
+      let itemInfo = null;
+      if(type === 1 || type === 2) {
+        itemInfo = (
+          <Item key={item.id}>
+            <Item.Image size='small' src='http://47.96.21.88:8086/public/1.png' />
+            <Item.Content verticalAlign='middle'>
+              <Item.Header className='info-title'>{item.info_title}</Item.Header>
+              <Item.Meta>
+                <span className='price'>$1200</span>
+                <span className='stay'>1 Month</span>
+              </Item.Meta>
+            </Item.Content>
+          </Item>
+        );
+      }
+      listContent.push(itemInfo);
+    });
+    return listContent;
+  }
+
+  render() {
+    let { hasMore, initializing } = this.state;
+    let { type } = this.props;
+    return (
+      <div className="view">
+        <Tloader className="main"
+          onRefresh={this.refresh}
+          onLoadMore={this.loadMore}
+          hasMore={hasMore}
+          initializing={initializing}>
+          <ul>
+            {this.produceList(type)}
+          </ul>
+        </Tloader>
+      </div>
+    );
+  }
+}
+
+export default LoadMore;
