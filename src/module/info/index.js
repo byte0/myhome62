da72ac8f1acb80c67aac46a80ab8d3587aad77a1
code1031 <1031247267@qq.com>
2019-03-14 15:56:24 +0800

    完成资讯模块基本布局

--- src/module/info/index.js
+++ src/module/info/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Tab } from 'semantic-ui-react';
+import './index.css';
 
 class Info extends React.Component {
   render() {
+    const panes = [
+      { menuItem: '资讯', render: () => <Tab.Pane>资讯</Tab.Pane>},
+      { menuItem: '头条', render: () => <Tab.Pane>头条</Tab.Pane>},
+      { menuItem: '问答', render: () => <Tab.Pane>问答</Tab.Pane>}
+    ];
     return (
-      <div>
-        资讯
+      <div className='find-container'>
+        <div className="find-topbar">资讯</div>
+        <div className="find-content">
+          <Tab panes={panes}/>
+        </div>
       </div>
     );
   }
