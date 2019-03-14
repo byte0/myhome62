e2ca8cc7c7b2231c75c72a915cdb88e5dc5723ac
code1031 <1031247267@qq.com>
2019-03-14 16:22:35 +0800

    完成列表数据的动态填充

--- src/module/info/index.js
+++ src/module/info/index.js
@@ -1,13 +1,78 @@
 import React from 'react';
-import { Tab } from 'semantic-ui-react';
+import { Tab, Item } from 'semantic-ui-react';
 import './index.css';
+import axios from 'axios';
+
+class InfoContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      infoData: [],
+      pagenum: 0,
+      pagesize: 2,
+      type: 1
+    }
+  }
+  componentDidMount = async () => {
+    // 加载数据
+    let ret = await axios.post('infos/list', {
+      pagenum: this.state.pagenum,
+      pagesize: this.state.pagesize,
+      type: this.state.type
+    });
+    this.setState({
+      infoData: ret.data.data.list.data
+    });
+  }
+  render() {
+    let infoContent = this.state.infoData.map(item=>{
+      return (
+        <Item key={item.id}>
+          <Item.Image size='small' src='http://47.96.21.88:8086/public/1.png' />
+          <Item.Content verticalAlign='middle'>
+            <Item.Header className='info-title'>{item.info_title}</Item.Header>
+            <Item.Meta>
+              <span className='price'>$1200</span>
+              <span className='stay'>1 Month</span>
+            </Item.Meta>
+          </Item.Content>
+        </Item>
+      );
+    });
+    return (
+      <div>
+        {infoContent}
+      </div>
+    );
+  }
+}
+
+class TopContent extends React.Component {
+  render() {
+    return (
+      <div>
+        头条内容
+      </div>
+    );
+  }
+}
+
+class FaqContent extends React.Component {
+  render() {
+    return (
+      <div>
+        问答内容
+      </div>
+    );
+  }
+}
 
 class Info extends React.Component {
   render() {
     const panes = [
-      { menuItem: '资讯', render: () => <Tab.Pane>资讯</Tab.Pane>},
-      { menuItem: '头条', render: () => <Tab.Pane>头条</Tab.Pane>},
-      { menuItem: '问答', render: () => <Tab.Pane>问答</Tab.Pane>}
+      { menuItem: '资讯', render: () => <Tab.Pane><InfoContent/></Tab.Pane>},
+      { menuItem: '头条', render: () => <Tab.Pane><TopContent/></Tab.Pane>},
+      { menuItem: '问答', render: () => <Tab.Pane><FaqContent/></Tab.Pane>}
     ];
     return (
       <div className='find-container'>
