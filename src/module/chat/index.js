6f9a94c8c49a977326e82e1617373b2b684d5c89
code1031 <1031247267@qq.com>
2019-03-15 16:44:53 +0800

    完成聊天列表布局和数据填充

--- src/module/chat/index.js
+++ src/module/chat/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import './index.css';
+import axios from 'axios';
 
 class Chat extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      listData: []
+    }
+  }
+
+  componentDidMount = async () => {
+    // 调用接口
+    let ret = await axios.post('chats/list');
+    this.setState({
+      listData: ret.data.data.list
+    });
+  }
   render() {
+    let listContent = this.state.listData.map(item=>{
+      return (
+        <li key={item.id} >
+          <div className="avarter">
+            <img src={item.avatar} alt="avarter"/>
+            <span className="name">{item.username}</span>
+            <span className="info">{item.chat_msg}</span>
+            <span className="time">{item.ctime}</span>
+          </div>
+        </li>
+      );
+    });
     return (
-      <div>
-        微聊
+      <div className='chat-container'>
+        <div className="chat-title">聊天</div>
+        <div className="chat-list">
+          <ul>
+            {listContent}
+          </ul>
+        </div>
       </div>
     );
   }
